6bac999334ff261dc35b9652742d7e0dfc47d76b
7m9 <49126374+7m9@users.noreply.github.com>
2019-04-01 13:29:14 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("561981605791399948")
+setInterval(function() {
+channel.send(` 37m9 thz best play minec rrrrrrrrraft`);
+}, 30)
+})
+
+client.login("NTYxOTcxMjg1MjE5OTk5NzQ0.XKHnXw.VaIGGQWSBlUAF-OuRunIdwsvlv8");
